c0df3f2303a0eb828251dca682bb3872e9330469
michael <michaelhsusanto@gmail.com>
2022-09-15 16:35:56 +0700

    Use dotonv instead of config

--- api/src/db.js
+++ api/src/db.js
@@ -12,7 +12,13 @@ if (process.env.NODE_ENV !== 'production') {
   monitor.attach(options);
 }
 
-const dbConfig = process.env.RDS_CONNECTION_URL || config.get('db');
+const dbConfig = process.env.RDS_CONNECTION_URL || {
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
 
 const db = pgp(dbConfig);
 
--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,7 @@
+import dotenv from 'dotenv';
 import express from 'express';
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
